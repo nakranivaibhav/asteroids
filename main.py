401b1056cc86f27ace950e39cb311de51aa670df
nakranivaibhav <nakranivaibhav14@gmail.com>
2025-04-25 12:12:59 +0530

    baby steps

---
+++ main.py
@@ -0,0 +1,25 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import (
+    SCREEN_WIDTH, 
+    SCREEN_HEIGHT, 
+    ASTEROID_SPAWN_RATE, 
+    ASTEROID_MIN_RADIUS,
+    ASTEROID_KINDS,
+    ASTEROID_MAX_RADIUS
+    )
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
